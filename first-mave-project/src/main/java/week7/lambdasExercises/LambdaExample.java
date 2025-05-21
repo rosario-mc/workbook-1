4a4e50d32b2b264c00eb71e72c4462071893e631
Rosario <RMC>
2025-05-21 13:45:00 -0700

    Continued

--- first-mave-project/src/main/java/week7/lambdasExercises/LambdaExample.java
+++ first-mave-project/src/main/java/week7/lambdasExercises/LambdaExample.java
@@ -14,12 +14,14 @@ public class LambdaExample {
         UnaryOperator square = (a) -> (int) Math.pow(a, 2);
         System.out.println(square.apply(5));
 
-        TriFunction average = (a, b, c) -> (a + b + c) / 3;
+        TriFunction average = (a, b, c) -> (a + b + c) / 3.0;
         System.out.println(average.apply(5, 4, 5));
 
         Printer print = (s) -> System.out.println("Print text: " + s);
         print.printer("This is the text I added.");
 
-        Predicate test = ()
+        Predicate<String> test = (s) -> s.isEmpty();
+        System.out.println(test.test("hi"));
+        System.out.println(test.test(""));
     }
 }
