3b57b0cd9eed2a95df823510cdbc36b56283f25d
Rosario <RMC>
2025-06-09 14:07:04 -0700

    Continued SQL Exercise

--- first-mave-project/src/main/java/Mod03/Northwind.java
+++ first-mave-project/src/main/java/Mod03/Northwind.java
@@ -2,10 +2,38 @@ package Mod03;
 
 import java.sql.*;
 
+
+//WITHOUT TRY/CATCH
 public class Northwind {
     public static void main(String[] args) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "yearup");
 
+        if (args.length != 2) {
+            System.out.println("Application needs 2 parameters to run.");
+        }
+
+        String username = args[0];
+        String password = args[1];
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);
+
+        PreparedStatement ps = connection.prepareStatement("SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM northwind.products;");
+
+        ResultSet rs = ps.executeQuery();
+
+        System.out.printf("%50s %n%-5s %-35s %-15s %-10s%n","ALL PRODUCTS IN NORTHWIND DATABASE", "ID", "PRODUCT NAME", "UNIT PRICE", "UNITS IN STOCK");
+
+        while (rs.next()){
+            System.out.printf("%-5s %-35s %-15s %-10s%n", rs.getString("ProductID"), rs.getString("ProductName"), rs.getString("UnitPrice"), rs.getString("UnitsInStock"));
+        }
+
+        rs.close();
+        ps.close();
+        connection.close();
+
+    }
+}
+        /*
+        Exercise 1
         Statement statement = connection.createStatement();
 
         String query = "SELECT * FROM products";
@@ -19,5 +47,5 @@ public class Northwind {
             System.out.println(products);
         }
         connection.close();
-    }
-}
+
+         */
